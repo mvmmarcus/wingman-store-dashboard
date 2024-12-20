d90feaf4b961b0bb6e1dfdf46cdd7b278437667f
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2024-12-20 15:25:29 -0300

    fix: adjust build

--- src/shared/di/modules/controller.module.ts
+++ src/shared/di/modules/controller.module.ts
@@ -1,16 +1,8 @@
 import { createModule } from "@evyweb/ioctopus";
 import { DISymbol } from "src/shared/di/symbols";
-import { UserController } from "src/layers/interface-adapters/controllers/user.controller";
 
 export function createControllerModule() {
   const moduleService = createModule();
-
-  moduleService
-    .bind(DISymbol.UserController)
-    .toClass(UserController, [
-      DISymbol.IGetUserByNameUsecase,
-      DISymbol.ICrashlyticsService,
-    ]);
 
   return moduleService;
 }
--- src/shared/di/symbols.ts
+++ src/shared/di/symbols.ts
@@ -1,8 +1,5 @@
-import { IUserRepository } from "src/layers/application/interfaces/repositories/iuser.respository.interface";
 import { ICrashlyticsService } from "src/layers/application/interfaces/services/crashlytics.service.interface";
 import { IHttpService } from "src/layers/application/interfaces/services/http.service.interface";
-import { IGetUserByNameUsecase } from "src/layers/application/interfaces/usecases/get-user-by-name.usecase.interface";
-import { UserController } from "src/layers/interface-adapters/controllers/user.controller";
 
 export const DISymbol = {
   // Services
@@ -10,13 +7,10 @@ export const DISymbol = {
   ICrashlyticsService: Symbol.for("ICrashlyticsService"),
 
   // Repositories
-  IUserRepository: Symbol.for("IUserRepository"),
 
   // Use Cases
-  IGetUserByNameUsecase: Symbol.for("IGetUserByNameUsecase"),
 
   // Controllers
-  UserController: Symbol.for("UserController"),
 };
 
 export type DIValue = {
@@ -25,11 +19,8 @@ export type DIValue = {
   ICrashlyticsService: ICrashlyticsService;
 
   // Repositories
-  IUserRepository: IUserRepository;
 
   // Use Cases
-  IGetUserByNameUsecase: IGetUserByNameUsecase;
 
   // Controllers
-  UserController: UserController;
 };
